58b0bc6768d058b66d884b6c9f165c40dd788f46
abdullah ahmad <abdoehsan69@gmail.com>
2025-07-29 02:56:07 +0300

    Create users.js

---
+++ users.js
@@ -0,0 +1,5 @@
+module.exports = [
+  { email: "alice.admin@gmail.com", password: "admin123", redirect: "/admin.html" },
+  { email: "bob.staff@gmail.com", password: "staff123", redirect: "/staff.html" },
+  { email: "carol.customer@gmail.com", password: "customer123", redirect: "/customer.html" }
+];
