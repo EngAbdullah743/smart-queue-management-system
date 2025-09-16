1b8117f3a29768329f890dd6bbc7435580901ef8
abdullah ahmad <abdoehsan69@gmail.com>
2025-09-17 02:26:44 +0300

    Update and rename hash.js to utils/hash.js

--- utils/hash.js
+++ utils/hash.js
@@ -1,15 +1,11 @@
-// utils/hash.js
 const bcrypt = require('bcrypt');
 
-// 1) Define hashPassword
 const hashPassword = async (password) => {
   return bcrypt.hash(password, 10);
 };
 
-// 2) Define comparePassword
 const comparePassword = async (plainText, hash) => {
   return bcrypt.compare(plainText, hash);
 };
 
-// 3) Export both functions
 module.exports = { hashPassword, comparePassword };
